aaa397ba38616f18702be91a8ed620be82ca6d02
Rafael  Castro <rafaelhenriquec@gmail.com>
2018-10-28 18:17:27 -0300

    adicionado endpoint posts

--- src/main/java/com/trihappy/alpha/resources/UserResource.java
+++ src/main/java/com/trihappy/alpha/resources/UserResource.java
@@ -1,8 +1,6 @@
 package com.trihappy.alpha.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.trihappy.alpha.domain.Post;
 import com.trihappy.alpha.domain.User;
 import com.trihappy.alpha.dto.UserDTO;
 import com.trihappy.alpha.services.UserService;
@@ -59,6 +58,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
 
